08552b96f3fb6d31a02f5cbdb438cde41a73968e
DevanshiParekh <devanshiparekh63@gmail.com>
2018-03-07 23:46:30 +0530

    linking pages via backend

--- src/main/java/com/spring/app/web/controller/PageController.java
+++ src/main/java/com/spring/app/web/controller/PageController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-	// inject via application.properties
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
@@ -18,5 +17,29 @@ public class PageController {
 		model.put("message", this.message);
 		return "index";
 	}
-
+	@RequestMapping("/my-article")
+	public String myarticle(Map<String, Object> model) {
+		model.put("message", "my-article");
+		return "my-article";
+	}
+	@RequestMapping("/category")
+	public String category(Map<String, Object> model) {
+		model.put("message", "my-category");
+		return "category";
+	}
+	@RequestMapping("/login")
+	public String login(Map<String, Object> model) {
+		model.put("message", "login");
+		return "login";
+	}
+	@RequestMapping("/my-account")
+	public String myaccount(Map<String, Object> model) {
+		model.put("message", "my-account");
+		return "my-account";
+	}
+	
+	@RequestMapping("/index")
+	public String index(Map<String, Object> model) {
+		return this.welcome(model);
+	}
 }
