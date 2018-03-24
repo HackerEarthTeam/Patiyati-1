779faae3a1cfe5d04be7ba819562353179241d6b
Sivakumar ARUMUGAM <Sivakumar ARUMUGAM@wrathoracledistilled.com>
2018-03-24 13:47:51 +0530

    class file removed.

--- src/main/java/com/spring/app/SpringBootWebApplication.java
+++ src/main/java/com/spring/app/SpringBootWebApplication.java
@@ -14,7 +14,7 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootWebApplication.class);
 	}
-
+//C:\Users\Sivakumar ARUMUGAM\git\Patiyati-1\target\Patiyati-1.0.war
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
